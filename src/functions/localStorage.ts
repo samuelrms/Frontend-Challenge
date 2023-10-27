149f25c699d3bfcf20a5ce2adb1ad674210e808d
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-10-26 23:48:34 -0300

    fix: build error

--- src/functions/localStorage.ts
+++ src/functions/localStorage.ts
@@ -1,6 +1,9 @@
 import { ItemClothes } from '@/types/itemClothes';
 
-export const storageFavoritesClothes = localStorage.getItem('favorites');
+export const storageFavoritesClothes =
+  typeof localStorage !== 'undefined'
+    ? localStorage.getItem('favorites')
+    : null;
 
 export const saveStorageClothes = (favorites: ItemClothes[]) => {
   localStorage.setItem('favorites', JSON.stringify(favorites));
